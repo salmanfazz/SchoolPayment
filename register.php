54a7acd762dd2323de464927f5937838cda7a519
Salman <salman.fazzz@gmail.com>
2019-10-07 15:33:49 +0900

    New Login

--- register.php
+++ register.php
@@ -8,54 +8,125 @@ if ( isset($_POST["register"]) ) {
 		echo "
 			<script>
 				alert('Success');
+				document.location.href = 'login.php';
 			</script>
 			";
 	} else {
 		echo "
 			<script>
 				alert('Failed');
+				document.location.href = 'register.php';
 			</script>
 			";
 	}
 }
 ?>
 <html>
-	<head>
-		<title> Halaman Register </title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
-		<div id="login">
-			<h3 class="text-center text-white pt-5">Register</h3>
-			<div class="container">
-				<div id="login-row" class="row justify-content-center align-items-center">
-					<div id="login-column" class="col-md-6">
-						<div id="login-box" class="col-md-12">
-							<form id="login-form" class="form" action="" method="post">
-								<h3 class="text-center text-info">Register</h3>
-								<div class="form-group">
-									<label for="username" class="text-info">Username:</label><br>
-									<input type="text" name="username" id="username" class="form-control">
-								</div>
-								<div class="form-group">
-									<label for="password" class="text-info">Password:</label><br>
-									<input type="password" name="password" id="password" class="form-control">
-								</div>
-								<div class="form-group">
-									<label for="confirm" class="text-info">Confirm Password:</label><br>
-									<input type="password" name="confirm" id="confirm" class="form-control">
-								</div>
-								<div class="form-group">
-									<input type="submit" name="register" class="btn btn-info btn-md" value="Sign Up">
-								</div>
-								<div id="register-link" class="text-right">
-									<a href="login.php" class="text-info">Login</a>
-								</div>
-							</form>
-						</div>
+<head>
+	<title>Register</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+				<form class="login100-form validate-form flex-sb flex-w" method="post">
+					<span class="login100-form-title p-b-53">
+						Register
+					</span>
+					<div class="p-t-31 p-b-9">
+						<span class="txt1">
+							Username
+						</span>
 					</div>
-				</div>
+					<div class="wrap-input100 validate-input" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" >
+						<span class="focus-input100"></span>
+					</div>
+					
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Password
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" >
+						<span class="focus-input100"></span>
+					</div>
+					
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Confirm Password
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Confirm Password is required">
+						<input class="input100" type="password" name="confirm" >
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-17">
+						<button class="login100-form-btn" type="submit" name="register">
+							Sign Up
+						</button>
+					</div>
+
+					<div class="w-full text-center p-t-55">
+						<span class="txt2">
+							Have Account?
+						</span>
+
+						<a href="login.php" class="txt2 bo1">
+							Login
+						</a>
+					</div>
+				</form>
 			</div>
 		</div>
-	</body>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
 </html>
